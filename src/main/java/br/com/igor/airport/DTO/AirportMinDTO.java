9090a870004499297953cef166bdd73387bad6c9
Igor <igorcrispim154@gmail.com>
2025-12-04 15:52:24 -0300

    Término da Fase 4 -Add classe AiportMinDTO -Update interface AirportRepository – Implementado assinatura de método findByCountryIgnoreCase(); -Update classe AirportService – Implementado método findByCountry. -Update classe AirportController – Adicionado endpoint /country/{countryName}.

---
+++ src/main/java/br/com/igor/airport/DTO/AirportMinDTO.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.igor.airport.DTO;
+
+import br.com.igor.airport.entites.Airport;
+
+/**
+ *
+ * @author DIT2A
+ */
+public class AirportMinDTO {
+    private long id;
+    private String name;
+    private String city;
+
+    public AirportMinDTO(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+    private String iataCode;
+    
+    public AirportMinDTO() {
+        
+    }
+    
+    public AirportMinDTO(Airport airport) {
+        this.id = airport.getId();
+        this.name = airport.getName();
+        this.city = airport.getCity();
+        this.iataCode = airport.getIataCode();
+    }
+}
+
+
--- src/main/java/br/com/igor/airport/service/AirportService.java
+++ src/main/java/br/com/igor/airport/service/AirportService.java
@@ -1,5 +1,6 @@
 package br.com.igor.airport.service;
 
+import br.com.igor.airport.DTO.AirportMinDTO;
 import br.com.igor.airport.entites.Airport;
 import br.com.igor.airport.entites.AirportRepository;
 import java.util.List;
@@ -20,8 +21,19 @@ public class AirportService {
      }
      
              public List<Airport> findByCity(String city){
-            List<Airport> result =airportRepository.findByCityIgnoreCase(city);
+            List<Airport> result = airportRepository.findByCityIgnoreCase(city);
         return result;
        
+        
     }
- }
+       public List<AirportMinDTO> findByCountry(String country) {
+           List<Airport> resultAirport = airportRepository.findByCountryIgnoreCase(country);
+           
+           List<AirportMinDTO> resultDTO = resultAirport.stream()
+                   .map(x -> new AirportMinDTO(x)).toList();
+           
+           return resultDTO;
+       }
+ } 
+
+
